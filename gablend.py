73bc4ebdc7de63de43681e372640450bac7d6373
linuxuser0 <antiquescience@hotmail.com>
2011-12-06 13:07:12 -0800

    Update gablend.py

--- gablend.py
+++ gablend.py
@@ -1,13 +1,13 @@
 import bpy
 import random
 
-randomize(shape, dist):
+def randomize(shape, dist):
     for vert in shape.data.vertices:
         vert.co.x += random.randint(-dist,dist) 
         vert.co.y += random.randint(-dist,dist)
         vert.co.z += random.randint(-dist,dist)
 
-create_random_object():
+def create_random_object():
     bpy.ops.mesh.primitive_uv_sphere_add()
     randomize(bpy.data.objects["Sphere"], 2)
     
