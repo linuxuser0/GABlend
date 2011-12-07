1beb5dba0a013a07e21e5697a2bd3cd711fe6f84
linuxuser0 <antiquescience@hotmail.com>
2011-12-06 20:00:43 -0800

    added mutation of objects

--- gablend.py
+++ gablend.py
@@ -1,16 +1,27 @@
 import bpy
 import random
 
-def randomize(shape, dist):
-    for vert in shape.data.vertices:
-        vert.co.x += random.randint(-dist,dist) 
-        vert.co.y += random.randint(-dist,dist)
-        vert.co.z += random.randint(-dist,dist)
-
 def create_random_object(name, dist):
     bpy.ops.mesh.primitive_uv_sphere_add()
     random_object = bpy.context.active_object
-    randomize(random_object, dist)
+    randomize_object(random_object, dist)
     random_object.name = name
     return random_object
+
+def randomize_object(shape, dist, accuracy):
+    for vert in shape.data.vertices:
+        randomize_vertex(vert, dist, accuracy)
     
+def randomize_vertex(vert, dist, accuracy):
+    vert.co.x += random.randrange(-dist, accuracy, dist)
+    vert.co.y += random.randrange(-dist, accuracy, dist)
+    vert.co.z += random.randrange(-dist, accuracy, dist)
+    
+
+def mutate(obj, dist, accuracy):
+    verts = count_vertices(obj)
+    for vert in obj.data.vertices:
+        chance = random.randint(1, verts)
+        if chance == verts:
+            randomize_vertex(vert, dist, accuracy)
+            
