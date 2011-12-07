0b122b35eed1d49823a1ba76e50f2213e1ce6c1c
linuxuser0 <antiquescience@hotmail.com>
2011-12-06 20:22:59 -0800

    Update gablend.py

--- gablend.py
+++ gablend.py
@@ -1,27 +1,27 @@
 import bpy
 import random
 
-def create_random_object(name, dist, accuracy):
+def create_random_object(name, dist):
     bpy.ops.mesh.primitive_uv_sphere_add()
     random_object = bpy.context.active_object
-    randomize_object(random_object, dist, accuracy)
+    randomize_object(random_object, dist)
     random_object.name = name
     return random_object
 
-def randomize_object(shape, dist, accuracy):
+def randomize_object(shape, dist):
     for vert in shape.data.vertices:
-        randomize_vertex(vert, dist, accuracy)
+        randomize_vertex(vert, dist)
     
-def randomize_vertex(vert, dist, accuracy):
-    vert.co.x += random.randrange(-dist, dist, accuracy)
-    vert.co.y += random.randrange(-dist, dist, accuracy)
-    vert.co.z += random.randrange(-dist, dist, accuracy)
+def randomize_vertex(vert, dist):
+    vert.co.x += random.uniform(-dist, dist)
+    vert.co.y += random.uniform(-dist, dist)
+    vert.co.z += random.uniform(-dist, dist)
     
 
-def mutate(obj, dist, accuracy):
+def mutate(obj, dist):
     verts = count_vertices(obj)
     for vert in obj.data.vertices:
         chance = random.randint(1, verts)
         if chance == verts:
-            randomize_vertex(vert, dist, accuracy)
+            randomize_vertex(vert, dist)
             
